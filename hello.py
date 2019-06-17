a30e3947fa826fa34fcb8d8f1aa23dc4a52f1a55
Swapnil Jangam <neel@Swapnils-MacBook-Air.local>
2019-06-16 22:22:48 -0500

    Connection to db

--- hello.py
+++ hello.py
@@ -1,6 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 myapp = Flask(__name__)
+
+myapp.config['MYSQL_HOST'] = 'swapnil-jangam-server.database.windows.net'
+myapp.config['MYSQL_USER'] = 'swapniljangam'
+myapp.config['MYSQL_PASSWORD'] = 'Kingarhur4'
+myapp.config['MYSQL_DB'] = 'mydatabase'
+
+mysql = MySQL(myapp)
+
+def get_connection():
+    return mysql.connection
 
 @myapp.route("/")
 def hello():
-    return myapp.send_static_file("index.html")
+    conn = get_connection()
+    res = ''
+    if not conn:
+        res = 'No connection to db'
+    else:
+        res = 'DB connected'
+    return render_template("static/index.html", result=res)
+
+@myapp.route('/delete', methods=['GET'])
+def deleteall():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM earthquake")
+    conn.commit()
+    cur.close()
+    return "Successfully deleted all data"
