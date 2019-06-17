feba3dce0a4a8a7a1f56b8906daeee235e073a9f
Swapnil Jangam <neel@Swapnils-MacBook-Air.local>
2019-06-17 00:29:25 -0500

    Test database

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request
-#from flask_mysqldb import MySQL
+from flask_mysqldb import MySQL
 import os
 myapp = Flask(__name__)
 
-# myapp.config['MYSQL_HOST'] = 'swapnil-jangam-server.database.windows.net'
-# myapp.config['MYSQL_USER'] = 'swapniljangam@swapnil-jangam-server'
-# myapp.config['MYSQL_PASSWORD'] = 'Kingarhur4'
-# myapp.config['MYSQL_DB'] = 'mydatabase'
+myapp.config['MYSQL_HOST'] = 'swapnil-jangam-server.database.windows.net'
+myapp.config['MYSQL_USER'] = 'swapniljangam@swapnil-jangam-server'
+myapp.config['MYSQL_PASSWORD'] = 'Kingarhur4'
+myapp.config['MYSQL_DB'] = 'mydatabase'
 
-# mysql = MySQL(myapp)
+mysql = MySQL(myapp)
 port = int(os.getenv("PORT", 5000))
 
 # def get_connection():
@@ -16,12 +16,12 @@ port = int(os.getenv("PORT", 5000))
 
 @myapp.route("/")
 def hello():
-    # conn = get_connection()
+    conn = get_connection()
     res = ''
-    # if not conn:
-    #     res = 'No connection to db'
-    # else:
-    #     res = 'DB connected'
+    if not conn:
+        res = 'No connection to db'
+    else:
+        res = 'DB connected'
     return render_template("index.html", result=res)
 
 # @myapp.route('/delete', methods=['GET'])
