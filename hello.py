d3e0844386c59f5e7f69de77d809c1b362c05106
Swapnil Jangam <neel@Swapnils-MacBook-Air.local>
2019-06-17 00:01:20 -0500

    Test

--- hello.py
+++ hello.py
@@ -1,37 +1,37 @@
 from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
+#from flask_mysqldb import MySQL
 import os
 myapp = Flask(__name__)
 
-myapp.config['MYSQL_HOST'] = 'swapnil-jangam-server.database.windows.net'
-myapp.config['MYSQL_USER'] = 'swapniljangam@swapnil-jangam-server'
-myapp.config['MYSQL_PASSWORD'] = 'Kingarhur4'
-myapp.config['MYSQL_DB'] = 'mydatabase'
+# myapp.config['MYSQL_HOST'] = 'swapnil-jangam-server.database.windows.net'
+# myapp.config['MYSQL_USER'] = 'swapniljangam@swapnil-jangam-server'
+# myapp.config['MYSQL_PASSWORD'] = 'Kingarhur4'
+# myapp.config['MYSQL_DB'] = 'mydatabase'
 
-mysql = MySQL(myapp)
+# mysql = MySQL(myapp)
 port = int(os.getenv("PORT", 5000))
 
-def get_connection():
-    return mysql.connection
+# def get_connection():
+#     return mysql.connection
 
 @myapp.route("/")
 def hello():
-    conn = get_connection()
+    # conn = get_connection()
     res = ''
-    if not conn:
-        res = 'No connection to db'
-    else:
-        res = 'DB connected'
+    # if not conn:
+    #     res = 'No connection to db'
+    # else:
+    #     res = 'DB connected'
     return render_template("static/index.html", result=res)
 
-@myapp.route('/delete', methods=['GET'])
-def deleteall():
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute("DELETE FROM earthquake")
-    conn.commit()
-    cur.close()
-    return "Successfully deleted all data"
+# @myapp.route('/delete', methods=['GET'])
+# def deleteall():
+#     conn = get_connection()
+#     cur = conn.cursor()
+#     cur.execute("DELETE FROM earthquake")
+#     conn.commit()
+#     cur.close()
+#     return "Successfully deleted all data"
 
 # if __name__ == '__main__':
 #     myapp.run(host='0.0.0.0', port=port, debug=True)
