6be7168941a501bce011edd16f105292bd7d9acc
Swapnil Jangam <neel@Swapnils-MacBook-Air.local>
2019-06-17 15:56:28 -0500

    Test with delete route

--- hello.py
+++ hello.py
@@ -11,25 +11,27 @@ myapp.config['MYSQL_DB'] = 'mydatabase'
 mysql = MySQL(myapp)
 port = int(os.getenv("PORT", 5000))
 
+def get_connection():
+    return mysql.connection
 
 @myapp.route("/")
 def hello():
-    cur = mysql.connection.cursor()
+    conn = get_connection()
     res = 'Hi'
-    if not cur:
+    if not conn:
         res = 'No connection to db'
     else:
         res = 'DB connected'
     return render_template("index.html", result=res)
 
-# @myapp.route('/delete', methods=['GET'])
-# def deleteall():
-#     conn = get_connection()
-#     cur = conn.cursor()
-#     cur.execute("DELETE FROM earthquake")
-#     conn.commit()
-#     cur.close()
-#     return "Successfully deleted all data"
+@myapp.route('/delete', methods=['GET'])
+def deleteall():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM earthquake")
+    conn.commit()
+    cur.close()
+    return "Successfully deleted all data"
 
 # if __name__ == '__main__':
 #     myapp.run(host='0.0.0.0', port=port, debug=True)
